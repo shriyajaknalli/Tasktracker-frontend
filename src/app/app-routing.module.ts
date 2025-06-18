3cb4a347a049e8e1d516ebf156d491c7608bb77c
shriyajaknalli <shriya.jaknalli@gmail.com>
2025-06-18 15:20:24 -0700

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { BillComponent } from './pages/bill/bill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path: 'home',component:HomeComponent},
+  {path:'**', redirectTo: 'home', pathMatch: 'full'} , // Catch-all route
+  {path:'inventory', component: InventoryComponent},
+  {path: 'customer', component: CustomerComponent},
+  {path: 'bill', component:BillComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BillComponent } from './pages/bill/bill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent,
+    CustomerComponent,
+    HomeComponent,
+    BillComponent
   ],
   imports: [
     BrowserModule,
