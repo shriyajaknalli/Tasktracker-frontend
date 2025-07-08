938f615d78cb13d2ca4f2e1a9317f68c4f7ba7a4
shriyajaknalli <shriya.jaknalli@gmail.com>
2025-07-08 00:07:26 -0700

    update task.module to import shared module

--- src/app/features/task/task.module.ts
+++ src/app/features/task/task.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskListComponent } from './task-list/task-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     TaskListComponent
   ],
   imports: [
+      CommonModule,
+    SharedModule,
     CommonModule
   ],
   exports: [
