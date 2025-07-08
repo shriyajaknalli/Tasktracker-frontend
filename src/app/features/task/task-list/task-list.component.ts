8e64a6d54ae3982b437fff6522180f99401c52fb
shriyajaknalli <shriya.jaknalli@gmail.com>
2025-07-08 00:06:52 -0700

    task-list component update

--- src/app/features/task/task-list/task-list.component.ts
+++ src/app/features/task/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { TaskService } from 'src/app/core/services/task.service';
 import { Task } from 'src/app/core/models/task';
 
@@ -9,8 +9,10 @@ import { Task } from 'src/app/core/models/task';
 })
 export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
+    selectedTaskToEdit: Task | null = null;
+  showEditForm: boolean = false;
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.loadTasks();
@@ -22,10 +24,36 @@ export class TaskListComponent implements OnInit {
     });
   }
 
+
+  onCreateTask(newTask: Task): void {
+    this.taskService.createTask(newTask).subscribe((createdTask: Task) => {
+         console.log('Created Task:', createdTask); // Debugging
+   this.tasks = [...this.tasks, createdTask]; 
+   this.cdr.detectChanges(); // Add the created task to the tasks array
+  }, error => {
+    console.error('Error creating task:', error);
+    alert('Failed to create task. Please try again.');
+  });
+  }
+
+
+
   toggleCompleted(task: Task): void {
     const updatedTask = { ...task, completed: !task.completed };
     this.taskService.updateTask(updatedTask).subscribe(() => {
       task.completed = updatedTask.completed; // Update local UI
     });
   }
+
+  onEdit(task: Task) {
+    this.selectedTaskToEdit = task;
+    this.showEditForm = true;
+  }
+
+  onSaveEdit(updatedTask: Task) {
+  this.taskService.updateTask(updatedTask).subscribe(() => {
+   // reload the updated list
+    this.showEditForm = false;
+  });
+}
 }
