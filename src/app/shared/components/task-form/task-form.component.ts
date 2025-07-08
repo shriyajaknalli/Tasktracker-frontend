7107d573d0f754c651a845680b175e6a940eb884
shriyajaknalli <shriya.jaknalli@gmail.com>
2025-07-08 00:07:46 -0700

    update onsubmit

--- src/app/shared/components/task-form/task-form.component.ts
+++ src/app/shared/components/task-form/task-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskService } from 'src/app/core/services/task.service';
 import { Task } from 'src/app/core/models/task';
 
@@ -8,6 +8,9 @@ import { Task } from 'src/app/core/models/task';
   styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent {
+  @Input() taskToEdit: Task | null = null;
+@Input() mode: 'create' | 'edit' = 'create';
+@Output() formSubmit = new EventEmitter<Task>();
   task: Task = {
     title: '',
     description: '',
@@ -17,11 +20,30 @@ export class TaskFormComponent {
   constructor(private taskService: TaskService) {}
 
   onSubmit() {
-    this.taskService.createTask(this.task).subscribe(response => {
-      console.log('Task created:', response);
-      alert('Task added!');
-      this.resetForm();
-    });
+   if(this.mode === 'edit' && this.taskToEdit?.id){
+      this.taskService.updateTask({ ...this.task, id: this.taskToEdit.id }).subscribe(() => {
+        alert('Task updated!');
+        this.resetForm();
+        this.formSubmit.emit({ ...this.task, id: this.taskToEdit!.id });          
+      }, error => {
+        console.error('Error updating task:', error);
+        alert('Failed to update task. Please try again.');
+      })
+   
+    }
+    else
+    {
+      this.taskService.createTask(this.task).subscribe((createdTask: Task)=>{
+        alert('Task created!');
+        this.formSubmit.emit(createdTask);
+        console.log('Emitted Task:', createdTask);
+        this.resetForm();
+        
+      }, error => {
+        console.error('Error creating task:', error);
+        alert('Failed to create task. Please try again.');
+      })
+    }
   }
 
   private resetForm() {
